e23c92fc9dc60b0c8eb77dc558200aa273cfcf4f
Melara <105904994+Melara1024@users.noreply.github.com>
2023-01-11 14:24:02 +0900

    Code cleaning

--- src/main/java/ga/melara/stevesminipouch/Config.java
+++ src/main/java/ga/melara/stevesminipouch/Config.java
@@ -29,6 +29,19 @@ public class Config {
     public static ForgeConfigSpec.BooleanValue FORCE_ARMOR;
 
 
+    public static ForgeConfigSpec.BooleanValue REGISTER_INVENTORY;
+    public static ForgeConfigSpec.BooleanValue REGISTER_ARMOR;
+    public static ForgeConfigSpec.BooleanValue REGISTER_OFFHAND;
+    public static ForgeConfigSpec.BooleanValue REGISTER_CRAFT;
+    public static ForgeConfigSpec.BooleanValue REGISTER_INC1;
+    public static ForgeConfigSpec.BooleanValue REGISTER_INC9;
+    public static ForgeConfigSpec.BooleanValue REGISTER_INC27;
+    public static ForgeConfigSpec.BooleanValue REGISTER_DEC1;
+    public static ForgeConfigSpec.BooleanValue REGISTER_DEC9;
+    public static ForgeConfigSpec.BooleanValue REGISTER_DEC27;
+
+
+
     // client configs
     public static ForgeConfigSpec.IntValue RENDER_OFFSET_X;
     public static ForgeConfigSpec.IntValue RENDER_OFFSET_Y;
@@ -53,6 +66,18 @@ public class Config {
 
         DEFAULT_ARMOR = SERVER_BUILDER.comment("Default setting of armor activate").define("armor", true);
         FORCE_ARMOR = SERVER_BUILDER.comment("If true, The above settings are enforced on all players.").define("force_armor", false);
+
+        SERVER_BUILDER.comment("// item register settings");
+        REGISTER_INVENTORY = SERVER_BUILDER.comment("Register Inventory Item").define("register_inv", true);
+        REGISTER_ARMOR = SERVER_BUILDER.comment("Register Inventory Item").define("register_arm", true);
+        REGISTER_OFFHAND = SERVER_BUILDER.comment("Register Inventory Item").define("register_off", true);
+        REGISTER_CRAFT = SERVER_BUILDER.comment("Register Inventory Item").define("register_cft", true);
+        REGISTER_INC1 = SERVER_BUILDER.comment("Register Inventory Item").define("register_inc1", true);
+        REGISTER_DEC9 = SERVER_BUILDER.comment("Register Inventory Item").define("register_inc9", true);
+        REGISTER_INC27 = SERVER_BUILDER.comment("Register Inventory Item").define("register_inc27", true);
+        REGISTER_DEC1 = SERVER_BUILDER.comment("Register Inventory Item").define("register_dec1", true);
+        REGISTER_DEC9 = SERVER_BUILDER.comment("Register Inventory Item").define("register_dec9", true);
+        REGISTER_DEC27 = SERVER_BUILDER.comment("Register Inventory Item").define("register_dec27", true);
 
         SERVER_BUILDER.pop();
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_BUILDER.build());
