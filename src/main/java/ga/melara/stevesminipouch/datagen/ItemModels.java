88e58d15a4aeaaac0b8d098936bffbd9825a0c32
Melara <105904994+Melara1024@users.noreply.github.com>
2023-09-03 02:42:25 +0900

    replace textures and models(WIP)

--- src/main/java/ga/melara/stevesminipouch/datagen/ItemModels.java
+++ src/main/java/ga/melara/stevesminipouch/datagen/ItemModels.java
@@ -16,13 +16,24 @@ public class ItemModels extends ItemModelProvider {
     @Override
     protected void registerModels() {
         ResourceLocation parent = mcLoc("item/generated");
-        ResourceLocation box_1 = modLoc("item/" + SLOT_SUB1_ITEM.getId().getPath());
-        this.singleTexture(SLOT_SUB9_ITEM.getId().getPath(), box_1, "layer", modLoc("item/" + SLOT_SUB9_ITEM.getId().getPath()));
-        this.singleTexture(SLOT_SUB27_ITEM.getId().getPath(), box_1, "layer", modLoc("item/" + SLOT_SUB27_ITEM.getId().getPath()));
 
-        this.singleTexture(INVENTORY_ACTIVATE_ITEM.getId().getPath(), parent, "layer0", modLoc("item/" + INVENTORY_ACTIVATE_ITEM.getId().getPath()));
-        this.singleTexture(ARMOR_ACTIVATE_ITEM.getId().getPath(), parent, "layer0", modLoc("item/" + ARMOR_ACTIVATE_ITEM.getId().getPath()));
-        this.singleTexture(OFFHAND_ACTIVATE_ITEM.getId().getPath(), parent, "layer0", modLoc("item/" + OFFHAND_ACTIVATE_ITEM.getId().getPath()));
-        this.singleTexture(CRAFT_ACTIVATE_ITEM.getId().getPath(), parent, "layer0", modLoc("item/" + CRAFT_ACTIVATE_ITEM.getId().getPath()));
+        ResourceLocation cube = modLoc("item/cube");
+        ResourceLocation oriented_cube = modLoc("item/oriented_cube");
+        ResourceLocation backpack = modLoc("item/backpack");
+        ResourceLocation anvil = modLoc("item/anvil");
+        ResourceLocation arm = modLoc("item/arm");
+
+        this.singleTexture(SLOT_ADD1_ITEM.getId().getPath(), oriented_cube, "layer", modLoc("item/" + SLOT_ADD1_ITEM.getId().getPath()));
+        this.singleTexture(SLOT_ADD9_ITEM.getId().getPath(), oriented_cube, "layer", modLoc("item/" + SLOT_ADD9_ITEM.getId().getPath()));
+        this.singleTexture(SLOT_ADD27_ITEM.getId().getPath(), oriented_cube, "layer", modLoc("item/" + SLOT_ADD27_ITEM.getId().getPath()));
+
+        this.singleTexture(SLOT_SUB1_ITEM.getId().getPath(), cube, "layer", modLoc("item/" + SLOT_SUB1_ITEM.getId().getPath()));
+        this.singleTexture(SLOT_SUB9_ITEM.getId().getPath(), cube, "layer", modLoc("item/" + SLOT_SUB9_ITEM.getId().getPath()));
+        this.singleTexture(SLOT_SUB27_ITEM.getId().getPath(), cube, "layer", modLoc("item/" + SLOT_SUB27_ITEM.getId().getPath()));
+
+        this.singleTexture(INVENTORY_ACTIVATE_ITEM.getId().getPath(), backpack, "0", modLoc("item/backpack"));
+        this.singleTexture(ARMOR_ACTIVATE_ITEM.getId().getPath(), anvil, "layer0", modLoc("item/" + ARMOR_ACTIVATE_ITEM.getId().getPath()));
+        this.singleTexture(OFFHAND_ACTIVATE_ITEM.getId().getPath(), arm, "layer0", modLoc("item/" + OFFHAND_ACTIVATE_ITEM.getId().getPath()));
+        this.singleTexture(CRAFT_ACTIVATE_ITEM.getId().getPath(), oriented_cube, "layer", modLoc("item/" + CRAFT_ACTIVATE_ITEM.getId().getPath()));
     }
 }
